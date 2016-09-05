e9ca96d89a900310837e0b0d06f4d7f84043612d
Michael Casey <mcasey8540@gmail.com>
2016-09-05 11:52:01 -0400

    Add comments and four nested callback methods

---
+++ before-nested-callbacks.js
@@ -0,0 +1,29 @@
+'use strict';
+
+functionOne('parameterOne', function(error, result1){
+	if (error){
+		//handle error
+	} else {
+		//do something with the result
+		functionTwo(result1, function(error, result2){
+			if (error) {
+				//handle error
+			} else {
+				//do something with the result
+				functionThree(result2, function(error, result3){
+					if(error) {
+						// handle error
+					} else {
+						functionFour(result3, function(error, result4){
+							if(error) {
+								// handle error
+							} else {
+								// do something with the result..
+							}
+						});
+					}
+				});
+			}
+		});
+	}
+});
